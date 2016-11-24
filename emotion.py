297bce155520a6a6bfa896eb1a9d220fcc56d340
Jose Ignacio Gonzalez Cardenas <nacho.gon.car@gmail.com>
2016-11-24 11:52:29 -0600

    Enter VGG_S model.

--- emotion.py
+++ emotion.py
@@ -1,5 +1,5 @@
 from keras.models import Sequential
-from keras.layers import Dense
+from keras.layers import Dense, Convolution2D, MaxPooling2D, Activation
 import numpy
 
 seed = 13
@@ -7,19 +7,47 @@ numpy.random.seed(seed)
 
 # Load dataset
 
-
 # Create model
-model = Sequential()
-model.add(Dense(12, input_dim=8, init='uniform', activation='relu'))
-model.add(Dense(8, init='uniform', activation='relu'))
-model.add(Dense(1, init='uniform', activation='sigmoid'))
+def VVG_S(weight_path=None):
+    model = Sequential()
+    model.add(Convolution2D(96,7,7,input_shape=(1,256,256),border_model='valid')
+    model.add(Activation('relu')
+    model.add(MaxPooling2D(pool_size=(3, 3), strides=None, border_mode='valid', dim_ordering='default'))
+    model.add(Convolution2D(256,5,5,border_model='valid')
+    model.add(Activation('relu')
+    model.add(MaxPooling2D(pool_size=(2, 2), strides=None, border_mode='valid', dim_ordering='default'))
+    model.add(Convolution2D(512,3,3,border_model='valid')
+    model.add(Activation('relu')
+    model.add(Convolution2D(512,3,3,border_model='valid')
+    model.add(Activation('relu')
+    model.add(Convolution2D(512,3,3,border_model='valid')
+    model.add(Activation('relu')
+    model.add(MaxPooling2D(pool_size=(3, 3), strides=None, border_mode='valid', dim_ordering='default'))
 
-# Compile model
-#model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+    model.add(Flatten())
+    model.add(Dense(4048, activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(4048, activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(6, activation='softmax'))
+    
+    if weights_path:
+        model.load_weights(weight_path)
 
-#model.fit(X,Y, nb_epoch=1000, batch_size=10)
+    return model
 
-#scores = model.evaluate(X, Y)
-#print("%s: %.2f%%" % (model.metrics_names[1], scores[1]*100))
+
+model = VGG_S('vggs_weights.h5')
+model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+# Play with epoch value
+model.fit(X[train],Y[train], nb_epoch=100, batch_size=10)
+
+scores = model.evaluate(X[validate], Y[validate])
+print("%s: %.2f%%" % (model.metrics_names[1], scores[1]*100))
+              
+# At the end, use test dataset
+# scores = model.evaluate(X[test], Y[test])      
+# print("%s: %.2f%%" % (model.metrics_names[1], scores[1]*100))
 
 
